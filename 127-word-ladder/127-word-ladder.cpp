4f89a1edda05ad618e314af8ec8904108040d461
Mangesh Dalvi <dalvimangesh000@gmail.com>
2022-08-03 10:12:02 +0530

    Time: 1103 ms (29.39%), Space: 12.1 MB (97.54%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    bool check(string &s1,string &s2){
+        int n = (int) s1.size();
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(s1[i]!=s2[i]) cnt++;
+            if(cnt>1) return false;
+        }
+        return cnt==1;
+    }
+    
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        int n = (int) wordList.size();
+        
+        int level = 0;
+        
+        queue<string> q;
+        
+        vector<int> visited(n+2,-1);
+        
+        q.push(beginWord);
+        
+        while(q.size()){
+            
+            int curLevel = q.size();
+            
+            for(int i=0;i<curLevel;i++) {
+
+                string cur = q.front(); q.pop();
+
+                if(cur == endWord) {
+                    return level+1;
+                }
+                
+                for(int i=0;i<n;i++){
+
+                    if( check(cur,wordList[i]) and visited[i]==-1){
+                        q.push(wordList[i]);
+                        visited[i] = 1;
+                    }
+                }   
+            }
+            level++; 
+        }
+        
+        return 0;
+        
+    }
+};
