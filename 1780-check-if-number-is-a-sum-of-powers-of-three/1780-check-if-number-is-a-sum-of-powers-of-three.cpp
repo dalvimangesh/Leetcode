dcbd4b3a63484f26e4dbbbd56e64c608f4dcb677
Mangesh Dalvi <dalvimangesh000@gmail.com>
2022-08-22 07:28:10 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.16%) - LeetHub

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        
+        while( n ){
+            
+            if( n % 3 == 2 ){
+                
+                return false;
+                
+            }
+            
+            n /= 3;
+            
+        }
+        
+        return true;
+        
+    }
+};
